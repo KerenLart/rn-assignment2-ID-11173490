5aa44494a00fb9f271d30c298065c687a60b44bb
KerenLart <kklartey@st.ug.edu.gh>
2024-05-26 23:14:30 +0000

    updated the readme

--- blank-app/App.js
+++ blank-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is <Text style={{ fontWeight: 'bold' }}>Keren</Text></Text>
+      <Text style={styles.text}>My name is <Text style={{ fontWeight: 'bold' }} >Keren</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
